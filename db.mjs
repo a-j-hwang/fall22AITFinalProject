e7c38025db9fbaa24e35f8a3ef182c6ff8ce3aaa
a-j-hwang <alicia.jimin.h@gmail.com>
2022-11-09 14:16:29 +0900

    deleted db credentials

--- db.mjs
+++ db.mjs
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 const { Schema } = mongoose;
-const MONGODB = process.env.PORT || "mongodb+srv://jh6651:sSriSC4GuFQeeAij@cluster0.eztdrgk.mongodb.net/?retryWrites=true&w=majority";
+const MONGODB = process.env.PORT;
 mongoose.connect(MONGODB, (err, database) => {
   if (err) {
 console.log(err);
