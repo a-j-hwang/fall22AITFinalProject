30d8b1030e77ba570ed9ecb8a6df051d22de94e1
a-j-hwang <alicia.jimin.h@gmail.com>
2022-12-03 10:28:30 +0900

    trying to get vue workin...

--- app.mjs
+++ app.mjs
@@ -1,4 +1,52 @@
+
 import express from 'express'
+import cors from 'cors'
+import bodyParser from 'body-parser';
+import './db.mjs';
+import morgan from 'morgan';
+import mongoose from 'mongoose';
+
+const User = mongoose.model('User');
+const Table = mongoose.model('Table');
+const app = express();
+
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Deployed!'
+    });
+});
+
+app.get('/user', function(req, res) {
+    User.find(function(err, users, count) {
+        console.log(users);
+        res.json(users);
+		});
+
+});
+
+
+app.post('/user/create', function(req, res) {
+	console.log(req.body.username);
+	new User({
+		username: req.body.username,
+    password: req.body.password
+	}).save(function(err, users, count){
+		res.redirect('/user');
+	});
+});
+
+
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+    console.log(`listening on ${port}`);
+});
+
+/*import express from 'express'
 import path from 'path'
 import url from 'url';
 import './db.mjs';
@@ -16,14 +64,18 @@ const Table = mongoose.model('Table');
 //const unsplash="https://api.unsplash.com/photos/?client_id="+"R1sNeLMhp1aXODRnZt9eHUsAGwjuPz66OQd9Q6g7-Jw" /*process.env.ACCESSKEY;*/
 //console.log(unsplash);
 
+
+/*
+
 app.get('/', function(req, res) {
-    Table.find(function(err, titles, count) {
+  Table.find(function(err, titles, count) {
         console.log(titles);
         res.render( 'viewAll', {
       			titles:titles
       		});
 
 		});
+
 });
 
 app.post('/create', function(req, res) {
@@ -39,6 +91,7 @@ app.post('/create', function(req, res) {
 app.get('/create', function(req, res) {
   res.render('create');
 });
+
 
 
 app.get('/user', function(req, res) {
@@ -67,6 +120,11 @@ app.post('/user/create', function(req, res) {
 app.get('/user/create', function(req, res) {
   res.render('userCreate');
 });
+
+*/
+
+
+
 
 const PORT = process.env.PORT;
 
